56a12ecc5c61a994fdc5b401f533f52475ecd8a2
Jinson <jinson.nj@gmail.com>
2026-03-11 23:24:29 +0530

    updated to modular design

---
+++ image-tools/js/layout.js
@@ -0,0 +1,14 @@
+document.getElementById("sidebar").innerHTML = `
+
+<h3>Image Tools</h3>
+
+<a href="jpg-to-webp.html">JPG → WebP</a>
+<a href="png-to-jpg.html">PNG → JPG</a>
+<a href="webp-to-jpg.html">WebP → JPG</a>
+<a href="image-compressor.html">Compress</a>
+<a href="image-resizer.html">Resize</a>
+<a href="image-rotate.html">Rotate</a>
+<a href="image-crop.html">Crop</a>
+<a href="image-base64.html">Base64</a>
+
+`;
