aabe7f2f40f34eb06a997f40b4aeb2572058d66a
jinson-johny <60253883+jinson-johny@users.noreply.github.com>
2026-03-11 14:29:21 +0530

    Update core.js

--- image-tools/js/core.js
+++ image-tools/js/core.js
@@ -9,3 +9,41 @@ callback(img);
 }
 
 }
+
+function setupDrop(id,callback){
+
+let area=document.getElementById(id);
+
+area.addEventListener("dragover",(e)=>{
+
+e.preventDefault();
+
+});
+
+area.addEventListener("drop",(e)=>{
+
+e.preventDefault();
+
+let file=e.dataTransfer.files[0];
+
+callback(file);
+
+});
+
+area.addEventListener("click",()=>{
+
+let input=document.createElement("input");
+
+input.type="file";
+
+input.onchange=function(){
+
+callback(input.files[0]);
+
+}
+
+input.click();
+
+});
+
+}
