9fcf548e401911ace46c298cc6d5bf022f836c69
Jinson <jinson.nj@gmail.com>
2026-03-12 08:49:27 +0530

    multiple file conversion

--- image-tools/js/tool-ui.js
+++ image-tools/js/tool-ui.js
@@ -1,68 +1,69 @@
-function formatSize(bytes){
+function formatSize(bytes) {
 
-let kb = bytes/1024
+    let kb = bytes / 1024
 
-if(kb<1024)
-return kb.toFixed(2)+" KB"
+    if (kb < 1024)
+        return kb.toFixed(2) + " KB"
 
-return (kb/1024).toFixed(2)+" MB"
-
-}
-
-function setupTool(uploadId, callback){
-
-let area=document.getElementById(uploadId)
-
-area.addEventListener("dragover",(e)=>{
-e.preventDefault()
-})
-
-area.addEventListener("drop",(e)=>{
-e.preventDefault()
-
-let file=e.dataTransfer.files[0]
-
-processFile(file)
-
-})
-
-area.addEventListener("click",()=>{
-
-let input=document.createElement("input")
-
-input.type="file"
-
-input.onchange=()=>{
-processFile(input.files[0])
-}
-
-input.click()
-
-})
-
-function processFile(file){
-
-if(!file) return
-
-document.getElementById("upload-status").innerHTML =
-"Uploaded: "+file.name+
-"<br>Size: "+formatSize(file.size)
-
-document.getElementById("progress").style.width="40%"
-
-setTimeout(()=>{
-
-callback(file)
-
-document.getElementById("progress").style.width="100%"
-
-},600)
+    return (kb / 1024).toFixed(2) + " MB"
 
 }
+
+function setupTool(uploadId, callback) {
+
+    let area = document.getElementById(uploadId)
+
+    area.addEventListener("dragover", (e) => {
+        e.preventDefault()
+    })
+
+    area.addEventListener("drop", (e) => {
+        e.preventDefault()
+
+        let files=e.dataTransfer.files
+
+        processFile(files)
+
+    })
+
+    area.addEventListener("click", () => {
+
+        let input = document.createElement("input")
+
+        input.type="file"
+        input.multiple=true
+
+        input.onchange = () => {
+            processFile(input.files[0])
+        }
+
+        input.click()
+
+    })
+
+    function processFile(file) {
+
+        if (!file) return
+
+        document.getElementById("upload-status").innerHTML =
+            "Uploaded: " + file.name +
+            "<br>Size: " + formatSize(file.size)
+
+        document.getElementById("progress").style.width = "40%"
+
+        setTimeout(() => {
+
+            callback(file)
+
+            document.getElementById("progress").style.width = "100%"
+
+        }, 600)
+
+    }
 
 }
 
 /* Prevent browser from opening files when dropped */
 
-document.addEventListener("dragover",(e)=>e.preventDefault())
-document.addEventListener("drop",(e)=>e.preventDefault())
+document.addEventListener("dragover", (e) => e.preventDefault())
+document.addEventListener("drop", (e) => e.preventDefault())
