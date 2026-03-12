83598e244ecb11609efdef5e56629538c7b11b3b
Jinson <jinson.nj@gmail.com>
2026-03-12 10:04:58 +0530

    layout fixed

--- image-tools/js/layout.js
+++ image-tools/js/layout.js
@@ -1,14 +1,16 @@
+const toolPath = "../image-tools/tools/";
+
 document.getElementById("sidebar").innerHTML = `
 
 <h3>Image Tools</h3>
 
-<a href="jpg-to-webp.html">JPG → WebP</a>
-<a href="png-to-jpg.html">PNG → JPG</a>
-<a href="webp-to-jpg.html">WebP → JPG</a>
-<a href="image-compressor.html">Compress</a>
-<a href="image-resizer.html">Resize</a>
-<a href="image-rotate.html">Rotate</a>
-<a href="image-crop.html">Crop</a>
-<a href="image-base64.html">Base64</a>
+<a href="${toolPath}jpg-to-webp.html">JPG → WebP</a>
+<a href="${toolPath}png-to-jpg.html">PNG → JPG</a>
+<a href="${toolPath}webp-to-jpg.html">WebP → JPG</a>
+<a href="${toolPath}image-compressor.html">Compress</a>
+<a href="${toolPath}image-resizer.html">Resize</a>
+<a href="${toolPath}image-rotate.html">Rotate</a>
+<a href="${toolPath}image-crop.html">Crop</a>
+<a href="${toolPath}image-base64.html">Base64</a>
 
 `;
